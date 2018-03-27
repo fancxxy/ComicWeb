371e646dd9691f8d450d8f8fe2f6029908825cde
fancxxy <fancxxy@gmail.com>
2018-03-27 15:39:07 +0800

    add script for uwsgi and systemd

--- env.sh
+++ env.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
-export FLASK_APP=web.py
-export MYSQL_URL=mysql://username:password@localhost:3306/comic
-#export LD_LIBRARY_PATH=/alidata/server/mysql/lib:$LD_LIBRARY_PATH
+FLASK_APP=web.py
+MYSQL_URL=mysql://username:password@localhost:3306/comic_web
+#LD_LIBRARY_PATH=/alidata/server/mysql/lib:$LD_LIBRARY_PATH
